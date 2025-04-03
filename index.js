5c9f2d490a8e51951a75fed0887a6f1684528699
Chinmay <chinmaybadwaik9@gmail.com>
2025-04-03 17:52:08 +0530

    reseting code

--- index.js
+++ index.js
@@ -1,31 +1,15 @@
-const fs = require("fs");
+
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const connectDB = require("./db");
 const admin = require("firebase-admin");
+const authRoutes = require('./routes/authRoutes'); 
 const path = require('path'); 
 const app = express();
 const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_CREDENTIALS);
 const { GoogleAuth } = require('google-auth-library');
 
-
-
-const credentials = JSON.parse(Buffer.from(process.env.GOOGLE_APPLICATION_CREDENTIALS, "base64").toString());
-credentials.private_key = credentials.private_key.replace(/\\n/g, "\n");
-// Authenticate using GoogleAuth
-const auth = new GoogleAuth({
-  credentials,
-  scopes: ['https://www.googleapis.com/auth/cloud-platform'],
-});
-
-// Example: Get an auth client
-async function getAuthClient() {
-  const client = await auth.getClient();
-  console.log("Authenticated successfully!", client);
-}
-
-getAuthClient();
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -48,6 +32,7 @@ app.use('/api/auth', require("./routes/authRoutes")); // Mount the auth routes
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
 
 
 
